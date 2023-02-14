3a5d01e3bbc46cc7d6d5d7f7327e2ca87b8e7f95
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-02-15 06:52:46 +0900

    feat: add User model
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -1,1 +1,22 @@
-class User {}
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  factory User({
+    required String id,
+    required String email,
+    required String name,
+    required String picture,
+    required String locale,
+    @JsonKey(name: "verified_email") required bool verifiedEmail,
+    @JsonKey(name: "given_name") required String givenName,
+    @JsonKey(name: "family_name") required String familyName,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
