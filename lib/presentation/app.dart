e660d904b4bcd1636f693560588ecf10e61449c2
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-02-15 07:08:29 +0900

    refactor: import LoginPage absolute path
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ringdingdong/presentation/controllers/auth/auth_binding.dart';
 import 'package:ringdingdong/presentation/pages/google_login/google_login_page.dart';
+import 'package:ringdingdong/presentation/pages/login/login_page.dart';
 import 'package:ringdingdong/presentation/pages/main/main_page.dart';
-
-import 'pages/login/login_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
