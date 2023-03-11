fe7901538b667d62c7ea94f30a96fcef0d24454b
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-11 21:48:17 +0900

    refactor: refactor HomePage
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/controllers/home_controller.dart
+++ lib/presentation/controllers/home_controller.dart
@@ -1,60 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController
-    with GetSingleTickerProviderStateMixin {
+class HomeController extends GetxController with GetSingleTickerProviderStateMixin {
   RxString value = ''.obs;
   List<String> categoryList = ['카테고리1', '카테고리2', '카테고리3', '카테고리4'];
 
   String categoryName = '';
-  late List<Tab> myTabs = <Tab>[
-    Tab(
-      child: Text(
-        categoryList[0],
-        style: TextStyle(
-          //color: textDark,
-            fontFamily: 'IBMKR',
-            fontSize: 15,
-            fontWeight: FontWeight.w700),
-      ),
-    ),
-    Tab(
-      child: Text(
-        categoryList[1],
-        style: TextStyle(
-          //color: textDark,
-            fontFamily: 'IBMKR',
-            fontSize: 15,
-            fontWeight: FontWeight.w700),
-      ),
-    ),
-    Tab(
-      child: Text(
-        categoryList[2],
-        style: TextStyle(
-          //color: textDark,
-            fontFamily: 'IBMKR',
-            fontSize: 15,
-            fontWeight: FontWeight.w700),
-      ),
-    ),
-    Tab(
-      child: Text(
-        categoryList[3],
-        style: TextStyle(
-          //color: textDark,
-            fontFamily: 'IBMKR',
-            fontSize: 15,
-            fontWeight: FontWeight.w700),
-      ),
-    ),
-  ];
+  late List<Tab> myTabs = List<Tab>.generate(
+      categoryList.length,
+      (i) => Tab(
+              child: Text(
+            categoryList[i],
+            style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.w700),
+          )));
 
   late TabController tabController;
 
   @override
   void onInit() {
-    tabController = TabController(length: 4, vsync: this);
+    tabController = TabController(length: categoryList.length, vsync: this);
     super.onInit();
   }
 
--- lib/presentation/pages/main/home_page.dart
+++ lib/presentation/pages/main/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ringdingdong/app/config/app_colors.dart';
 import 'package:ringdingdong/presentation/controllers/home_controller.dart';
 import 'package:ringdingdong/presentation/pages/report_page.dart';
 import 'package:ringdingdong/presentation/pages/post_page.dart';
@@ -12,15 +13,13 @@ class HomePage extends GetView<HomeController> {
     Get.put(HomeController());
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xffFFE16F),
-          elevation: 0,
           leading: IconButton(
             onPressed: () {},
-            icon: Icon(Icons.question_mark),
+            icon: const Icon(Icons.question_mark),
           ),
-          actions: <Widget>[
+          actions: [
             IconButton(
-              icon: Icon(Icons.person),
+              icon: const Icon(Icons.person),
               onPressed: () {
                 Get.toNamed('/my_page');
               },
@@ -33,32 +32,26 @@ class HomePage extends GetView<HomeController> {
               builder: (_) {
                 return Column(
                   children: [
-                    Expanded(flex: 1, child: HomeHeader(context)),
-                    Expanded(flex: 6, child: HomeDetail()),
-                    Expanded(flex: 1, child: ReportButton()),
+                    Expanded(flex: 1, child: buildHomeHeaderWidget(context)),
+                    Expanded(flex: 6, child: buildHomeDetail()),
+                    Expanded(flex: 1, child: buildReportButton()),
                   ],
                 );
               }),
         ));
   }
 
-  Widget HomeHeader(BuildContext context) {
+  Widget buildHomeHeaderWidget(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         alignment: Alignment.center,
         child: ElevatedButton(
           onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            primary: Color(0xffFFE16F),
-            onPrimary: Color(0xffFFE16F),
-          ),
           child: Row(
             children: [
               Container(
-                margin: EdgeInsets.only(left: 20),
-                child: Text(
+                margin: const EdgeInsets.only(left: 20),
+                child: const Text(
                   '부전동',
                   style: TextStyle(
                     fontSize: 25,
@@ -69,8 +62,8 @@ class HomePage extends GetView<HomeController> {
               ),
               Expanded(child: Container()),
               Container(
-                margin: EdgeInsets.only(right: 20),
-                child: Text(
+                margin: const EdgeInsets.only(right: 20),
+                child: const Text(
                   '‣',
                   style: TextStyle(
                     fontSize: 30,
@@ -84,19 +77,19 @@ class HomePage extends GetView<HomeController> {
         ));
   }
 
-  Widget HomeDetail() {
+  Widget buildHomeDetail() {
     return Column(
       children: [
         Container(
           height: kToolbarHeight + 8.0,
           padding: const EdgeInsets.only(top: 16.0, right: 16.0, left: 16.0),
           decoration: BoxDecoration(
-            color: Color(0xffFFE16F),
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0)),
+            color: AppColors.primary,
+            borderRadius: const BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0)),
           ),
           child: TabBar(
             controller: controller.tabController,
-            indicator: BoxDecoration(
+            indicator: const BoxDecoration(
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0)), color: Colors.white),
             labelColor: Colors.black,
             unselectedLabelColor: Colors.grey.shade500,
@@ -108,40 +101,44 @@ class HomePage extends GetView<HomeController> {
             controller: controller.tabController,
             children: [
               Container(
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                   ),
                   child: ListView(
                     children: [
-                      for (int i = 0; i < 5; i++) ReportList(0),
+                      for (int i = 0; i < 5; i++) buildReportList(0),
+                      const SizedBox(height: 20.0),
                     ],
                   )),
               Container(
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                   ),
                   child: ListView(
                     children: [
-                      for (int i = 0; i < 5; i++) ReportList(1),
+                      for (int i = 0; i < 5; i++) buildReportList(1),
+                      const SizedBox(height: 20.0),
                     ],
                   )),
               Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.white,
                 ),
                 child: ListView(
                   children: [
-                    for (int i = 0; i < 5; i++) ReportList(2),
+                    for (int i = 0; i < 5; i++) buildReportList(2),
+                    const SizedBox(height: 20.0),
                   ],
                 ),
               ),
               Container(
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                   ),
                   child: ListView(
                     children: [
-                      for (int i = 0; i < 5; i++) ReportList(3),
+                      for (int i = 0; i < 5; i++) buildReportList(3),
+                      const SizedBox(height: 20.0),
                     ],
                   )),
             ],
@@ -151,41 +148,37 @@ class HomePage extends GetView<HomeController> {
     );
   }
 
-  Widget ReportList(int index) {
+  Widget buildReportList(int index) {
     return InkWell(
       onTap: () {
-        Get.to(() => PostPage());
+        Get.to(() => const PostPage());
       },
       child: Container(
         height: 120,
-        padding: EdgeInsets.all(15),
-        margin: EdgeInsets.only(
-          left: 20,
-          right: 20,
-          top: 20,
-        ),
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
         decoration: BoxDecoration(
           color: Colors.grey.shade200,
-          borderRadius: BorderRadius.all(Radius.circular(30)),
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
         ),
         child: Row(
           children: [
             Expanded(
                 flex: 5,
                 child: Container(
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   child: Column(
                     children: [
                       Container(
                         alignment: Alignment.centerLeft,
-                        child: Text(
+                        child: const Text(
                           '공사 안내',
                           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                         ),
                       ),
                       Container(
                         alignment: Alignment.centerLeft,
-                        child: Text('oo아파트 상가 공사 중 우회 바람'),
+                        child: const Text('oo아파트 상가 공사 중 우회 바람'),
                       ),
                     ],
                   ),
@@ -193,7 +186,7 @@ class HomePage extends GetView<HomeController> {
             Expanded(
                 flex: 2,
                 child: Container(
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   alignment: Alignment.topCenter,
                   child: Text(controller.categoryList[index]),
                 ))
@@ -203,37 +196,32 @@ class HomePage extends GetView<HomeController> {
     );
   }
 
-  Widget ReportButton() {
+  Widget buildReportButton() {
     return Container(
-        decoration: BoxDecoration(
-          color: Color(0xffFFE16F),
-        ),
-        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(color: AppColors.primary),
+        padding: const EdgeInsets.all(20),
         alignment: Alignment.center,
         child: ElevatedButton(
           onPressed: () {
-            Get.to(() => ReportPage());
+            Get.to(() => const ReportPage());
           },
           style: ElevatedButton.styleFrom(
-            elevation: 0,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            primary: Colors.white,
-            onPrimary: Colors.white,
+            backgroundColor: Colors.white,
           ),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(child: Container()),
-              Container(
+              Padding(
+                padding: const EdgeInsets.all(12.0),
                 child: Text(
                   '신고하기${controller.value}',
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
                   ),
                 ),
               ),
-              Expanded(child: Container()),
             ],
           ),
         ));
