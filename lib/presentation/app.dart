2f427a4fb81680f9e058581c6f75f4214dcd55f1
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-11 21:36:55 +0900

    refactor: remove old login elements
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ringdingdong/app/config/app_colors.dart';
 import 'package:ringdingdong/presentation/controllers/auth/auth_binding.dart';
-import 'package:ringdingdong/presentation/pages/google_login/google_login_page.dart';
 import 'package:ringdingdong/presentation/pages/login/login_page.dart';
 import 'package:ringdingdong/presentation/pages/login/select_service_page.dart';
 import 'package:ringdingdong/presentation/pages/login/term_page.dart';
-import 'package:ringdingdong/presentation/pages/main/main_page.dart';
 import 'package:ringdingdong/presentation/pages/mypage/my_page.dart';
 
 class App extends StatelessWidget {
@@ -38,9 +36,7 @@ class App extends StatelessWidget {
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
           )),
       routes: {
-        '/main': (context) => MainPage(),
         '/login': (context) => const LoginPage(),
-        '/google_login': (context) => const GoogleLoginPage(),
         '/my_page': (context) => const MyPage(),
         '/service_select': (context) => const SelectServicePage(),
         '/term': (context) => const TermPage(),
