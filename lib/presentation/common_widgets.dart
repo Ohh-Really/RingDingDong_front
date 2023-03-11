40a6c90c9396755e2e3f464bdf5b74db37ec712c
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-11 21:03:58 +0900

    refactor: add padding to CommonButton
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/common_widgets.dart
+++ lib/presentation/common_widgets.dart
@@ -8,15 +8,18 @@ class CommonButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: onPressed,
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(text,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ))));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          onPressed: onPressed,
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(text,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  )))),
+    );
   }
 }
