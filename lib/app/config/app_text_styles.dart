9e87908147bac57f8dbccacaf1f04db904bae6c0
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-12 15:45:03 +0900

    feat: add AppTextStyles
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/app_text_styles.dart
+++ lib/app/config/app_text_styles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
+  static TextStyle button = const TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
   static TextStyle header = const TextStyle(fontSize: 35, fontWeight: FontWeight.bold);
-  static TextStyle title = const TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle title = const TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle text = const TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
 }
