525592978e57ad84135bad4fed4104664fbc355e
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-16 21:32:57 +0900

    feat: initialize firebase
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ringdingdong/app/di/dependency.dart';
 import 'package:ringdingdong/app/services/secure_storage.dart';
 import 'package:ringdingdong/presentation/app.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   DependencyCreator.init();
@@ -12,5 +15,9 @@ void main() async {
 }
 
 initServices() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await Get.putAsync(() => SecureStorageService().init());
 }
